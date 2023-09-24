3f38518685f261021b24d170b8f04befb665c2e4
parkchaewon18 <0101angela@naver.com>
2023-09-25 01:01:43 +0900

    Rename: CareQApplication 파일 수정

--- src/main/java/com/reve/careQ/CareQApplication.java
+++ src/main/java/com/reve/careQ/CareQApplication.java
@@ -1,9 +1,11 @@
-package com.reve.careQ.standard;
+package com.reve.careQ;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CareQApplication {
 
 	public static void main(String[] args) {
