836b9846e74bc0b766323dedcae4583a9f91c69c
daineee424 <daineee424@gmail.com>
2024-01-05 03:00:56 +0900

    Refact: Message 서비스단 인터페이스 분리

--- src/main/java/com/reve/careQ/domain/Message/service/MessageService.java
+++ src/main/java/com/reve/careQ/domain/Message/service/MessageService.java
@@ -1,44 +1,18 @@
 package com.reve.careQ.domain.Message.service;
 
 import com.reve.careQ.domain.Chat.entity.Chat;
-import com.reve.careQ.domain.Message.entity.Message;
 import com.reve.careQ.domain.Message.dto.MessageDto;
-import com.reve.careQ.domain.Message.repository.MessageRepository;
+import com.reve.careQ.domain.Message.entity.Message;
 import com.reve.careQ.global.rsData.RsData;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-@Service
-@RequiredArgsConstructor
-@Transactional(readOnly = true)
-public class MessageService {
+public interface MessageService {
+    List<Message> findByChatId(Long chatId);
 
-    private final MessageRepository messageRepository;
+    Optional<LocalDateTime> findLastMessage(Long chatId);
 
-    public List<Message> findByChatId(Long chatId){return messageRepository.findByChatId(chatId);}
-
-    public Optional<LocalDateTime> findLastMessage(Long chatId){return messageRepository.findLastMessage(chatId);}
-
-    @Transactional
-    public RsData<Message> insert(MessageDto messageDto, Chat chat) {
-        Message message = createMessage(messageDto, chat);
-
-        return RsData.of("S-1", "메세지가 저장되었습니다.", message);
-    }
-
-    private Message createMessage(MessageDto messageDto, Chat chat){
-        Message message = Message
-                .builder()
-                .chat(chat)
-                .content(messageDto.getContent())
-                .userType(messageDto.getUserType().toString())
-                .build();
-
-        return messageRepository.save(message);
-    }
+    RsData<Message> insert(MessageDto messageDto, Chat chat);
 }
