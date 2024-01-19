378944e16068bf640bddfe780add3d5dab3df119
daineee424 <daineee424@gmail.com>
2024-01-19 18:11:27 +0900

    Refact: SubjectService 인터페이스 분리

---
+++ src/main/java/com/reve/careQ/domain/Subject/service/SubjectServiceImpl.java
@@ -0,0 +1,57 @@
+package com.reve.careQ.domain.Subject.service;
+
+import com.reve.careQ.domain.Subject.entity.Subject;
+import com.reve.careQ.domain.Subject.repository.SubjectRepository;
+import com.reve.careQ.global.rsData.RsData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SubjectServiceImpl implements SubjectService {
+
+    private final SubjectRepository subjectRepository;
+
+    public Optional<Subject> findById(Long id) {
+        return subjectRepository.findById(id);
+    }
+
+    public Optional<Subject> findByCode(String code) {
+        return subjectRepository.findByCode(code);
+    }
+
+    public Optional<Subject> findByName(String name) {
+        return subjectRepository.findByName(name);
+    }
+
+    @Transactional
+    public RsData<List<Subject>> find(String name) {
+
+        List<Subject> subjectList = subjectRepository.findByNameLike("%" + name + "%");
+
+        return RsData.of("S-1", "해당 이름을 포함한 과목을 찾았습니다.", subjectList);
+    }
+
+    @Transactional
+    public RsData<Subject> insert(String code, String name) {
+
+        Subject subject = createSubject(code, name);
+
+        return RsData.of("S-1", "과목테이블에 삽입되었습니다.", subject);
+    }
+
+    private Subject createSubject(String code, String name) {
+        Subject subject = Subject
+                .builder()
+                .code(code)
+                .name(name)
+                .build();
+
+        return subjectRepository.save(subject);
+    }
+}
