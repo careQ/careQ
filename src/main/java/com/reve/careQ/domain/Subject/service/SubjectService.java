d83fc5f20583eaad503b9be3ac6e8b10615d43aa
daineee424 <daineee424@gmail.com>
2024-01-23 18:10:43 +0900

    Fix: 과목 목록을 Admin에서 불러오도록 수정

--- src/main/java/com/reve/careQ/domain/Subject/service/SubjectService.java
+++ src/main/java/com/reve/careQ/domain/Subject/service/SubjectService.java
@@ -3,13 +3,11 @@ package com.reve.careQ.domain.Subject.service;
 import com.reve.careQ.domain.Subject.entity.Subject;
 import com.reve.careQ.global.rsData.RsData;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface SubjectService {
     Optional<Subject> findById(Long id);
     Optional<Subject> findByCode(String code);
     Optional<Subject> findByName(String name);
-    RsData<List<Subject>> find(String name);
     RsData<Subject> insert(String code, String name);
 }
